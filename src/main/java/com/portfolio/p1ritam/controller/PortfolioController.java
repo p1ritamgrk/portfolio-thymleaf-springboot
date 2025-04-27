385f994a2970891244d35f0ad15e5148567d1264
p1ritam__01 <pritam.shrestha@codehimalaya.net>
2025-04-28 00:49:44 +0545

    added portfolio pages

--- src/main/java/com/portfolio/p1ritam/controller/PortfolioController.java
+++ src/main/java/com/portfolio/p1ritam/controller/PortfolioController.java
@@ -1,4 +1,13 @@
 package com.portfolio.p1ritam.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PortfolioController {
+
+    @GetMapping()
+    public String  getHomePage() {
+        return "index";
+    }
 }
